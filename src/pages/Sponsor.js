563d1ea84d399e52e4902846969f439b01350786
Arunabha-NOTE <arunabham2017@gmail.com>
2023-09-09 02:40:08 +0530

    Changed buttons

--- src/pages/Sponsor.js
+++ src/pages/Sponsor.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SponsorCard from "../components/SponsorCard";
+import Navbar from "../components/Navbar";
 
 function Sponsor() {
   return (
@@ -7,9 +8,9 @@ function Sponsor() {
         <div className='flex justify-center bg-[#00629b] text-opacity-90 h-[10vh]'>
             Navbar
         </div>
-        <div className="flex flex-col items-center justify-center my-[2%]">
+        <div className='flex flex-col items-center justify-center my-[2%]'>
             <div>
-                <h1 className="text-6xl text-center text-transparent bg-clip-text bg-gradient-to-tl from-blue-950 to-blue-500 font-extrabold pb-[5%]">
+                <h1 className='text-6xl text-center text-transparent bg-clip-text bg-gradient-to-tl from-blue-950 to-blue-500 font-extrabold pb-[5%]'>
                     SPONSORS
                 </h1>
             </div>
