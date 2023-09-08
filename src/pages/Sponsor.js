95ea8e25577eabee509cf90d33f7395fef9a0d8c
Arunabha-NOTE <arunabham2017@gmail.com>
2023-09-09 02:46:14 +0530

    Added bg color

--- src/pages/Sponsor.js
+++ src/pages/Sponsor.js
@@ -5,16 +5,16 @@ import Navbar from "../components/Navbar";
 function Sponsor() {
   return (
     <>
-        <div className='flex justify-center bg-[#00629b] text-opacity-90 h-[10vh]'>
-            Navbar
-        </div>
-        <div className='flex flex-col items-center justify-center my-[2%]'>
-            <div>
-                <h1 className='text-6xl text-center text-transparent bg-clip-text bg-gradient-to-tl from-blue-950 to-blue-500 font-extrabold pb-[5%]'>
-                    SPONSORS
-                </h1>
+        <div className='bg-[#e3ebfa]'>
+            <Navbar />
+            <div className='flex flex-col items-center justify-center my-[2%]'>
+                <div>
+                    <h1 className='text-6xl text-center text-transparent bg-clip-text bg-gradient-to-tl from-blue-950 to-blue-500 font-extrabold pb-[5%]'>
+                        SPONSORS
+                    </h1>
+                </div>
+                <SponsorCard/>
             </div>
-            <SponsorCard/>
         </div>
     </>
   )
