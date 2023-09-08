668c4fc50927ebb841b7bd2316499bf430ad549f
Arunabha-NOTE <arunabham2017@gmail.com>
2023-09-09 04:05:59 +0530

    removed unused code

--- src/pages/Track.js
+++ src/pages/Track.js
@@ -8,20 +8,15 @@ import "aos/dist/aos.css";
 
 function Track() {
     AOS.init({
-        disable: false,
         startEvent: 'DOMContentLoaded',
         initClassName: 'aos-init',
         animatedClassName: 'aos-animate',
-        useClassNames: false,
-        disableMutationObserver: false,
         debounceDelay: 50,
         throttleDelay: 99,
         offset: 120,
         delay: 200,
         duration: 800,
         easing: 'ease',
-        once: false,
-        mirror: false,
         anchorPlacement: 'top-bottom',
 
     });
