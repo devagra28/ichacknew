28092d1c777b48ecc50a03d95e3f56da359a6f8f
Arunabha-NOTE <arunabham2017@gmail.com>
2023-09-09 04:45:39 +0530

    motion added and changes to text

--- src/components/SponsorCard.js
+++ src/components/SponsorCard.js
@@ -11,8 +11,8 @@ const SponsorCard = () => {
                     <div className="flex flex-col items-center justify-around border border-gray-300 rounded-lg md:flex-row mb-4 bg-[#e3ebfa] md:p-8 mx-[1%] md:mx-[5%]">
                         <img className="inline max-h-full max-w-full align-middle justify-between object-cover w-full rounded-t-lg h-full md:h-full md:w-96 md:rounded-none md:rounded-l-lg p-[1%]" src={sponsor.sponsorImage} alt=""/>
                         <div className="flex flex-col justify-between p-4 leading-normal">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center md:text-left">{sponsor.sponsorName}</h5>
-                            <p className="mb-3 font-normal text-gray-700 text-justify collapse md:visible">{sponsor.sponsorAbout}</p>
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">{sponsor.sponsorName}</h5>
+                            <p className="mb-3 font-normal text-gray-700 text-justify">{sponsor.sponsorAbout}</p>
                             <div className='pb-[1%]'>
                                 <a href="/">
                                     <CustomButton
--- src/pages/Sponsor.js
+++ src/pages/Sponsor.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import SponsorCard from "../components/SponsorCard";
 import Navbar from "../components/Navbar";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function Sponsor() {
+    AOS.init({
+        startEvent: 'DOMContentLoaded',
+        initClassName: 'aos-init',
+        animatedClassName: 'aos-animate',
+        debounceDelay: 50,
+        throttleDelay: 99,
+        offset: 120,
+        delay: 200,
+        duration: 800,
+        easing: 'ease',
+        anchorPlacement: 'top-bottom',
+
+    });
   return (
     <>
         <div className='bg-[#e3ebfa]'>
             <Navbar />
-            <div className='flex flex-col items-center justify-center'>
+            <div className='flex flex-col items-center justify-center' data-aos="fade-up">
                 <div>
                     <h1 className='text-6xl text-center font-[#5A6473] font-bold mb-[5vh]'>
                         SPONSORS
