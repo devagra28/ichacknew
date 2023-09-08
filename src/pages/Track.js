3931b9927e4ad30acb59d678ec8b23ddd61928be
Arunabha-NOTE <arunabham2017@gmail.com>
2023-09-09 03:08:19 +0530

    Changes to padding

--- src/pages/Track.js
+++ src/pages/Track.js
@@ -6,7 +6,7 @@ function Track() {
   return (
     <div className='bg-[#e3ebfa]'>
         <Navbar />
-        <div className='px-[5%] '>
+        <div className='px-[1%] '>
             <div className='justify-center pb-[5%]'>
                 <h1 className='text-6xl text-center text-transparent bg-clip-text bg-gradient-to-tl from-blue-950 to-blue-500 font-extrabold'>TRACKS</h1>
             </div>
